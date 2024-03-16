f8b6a70345604ff993cd3453fb09158e1660d63d
jh2946 <junhong2946@gmail.com>
2024-03-16 22:34:56 +0800

    added task component, properties

--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
